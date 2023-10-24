0b6f042aca33d7103953122d615162e8c5404b63
Leonie Chamberlain <Leoniechamberlain@gmail.com>
2023-10-24 13:09:04 +0100

    Table/Column Structure
    Re-structured the table/column classes.

--- Code/DB-Project/src/Column.java
+++ Code/DB-Project/src/Column.java
@@ -1,33 +1,30 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-//Manages a hashmap of columns for one specific data type.
 public class Column<T> {
-    private Map<String, ArrayList<T>> columns  = new HashMap<String, ArrayList<T>>(); //Key=column name, value = list of entry's values
-    private String type; //Type it stores (int, float, string etc)
+    private String type;
+    private Map<Integer, T> entries  = new HashMap<Integer, T>();
+    private boolean isNew; //Dictates whether it is added to the database
 
-    public Column(String type){
-        this.columns.put(type, new ArrayList<T>());
+    public Column(String type, boolean isNew) {
+        this.type = type;
+        this.isNew = isNew;
+    }
+
+    public void addEntry(int index, T value){
+        entries.put(index, value);
     }
 
     //Edits singular entry value
-    public void editValue(String column, int index, T newValue){
-        columns.computeIfPresent(column, (key, value) -> {
-            value.set(index, newValue);
-            return value;
-        });
+    public void editValue(int index, T newValue){
+        entries.put(index, newValue);
     }
 
-    //Returns a string of the variable type
+    //Getters
     public String getType(){
         return this.type;
     }
-
-    //Adds new column
-    public void addColumn(String name){
-        columns.put(name, new ArrayList<T>());
-    }
-
+    public int size(){ return entries.size();}
+    public boolean containsKey (int ID) {return entries.containsKey(ID);}
 }
--- Code/DB-Project/src/Table.java
+++ Code/DB-Project/src/Table.java
@@ -1,33 +1,43 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 //Manages a table
 public class Table<T> {
-    //Old approaches vv
-    //private List<Map<String, String>> columns = new ArrayList<>();
-    //private List<HashMap<String, List<?>>> Columns = new ArrayList<>();
-    //New approach vv
-    private List<Column> columns = new ArrayList<>(); //list of column hashmaps (hashmaps of column type <column name, list(entry values)>)
-    private Map<String, Integer> indexes  = new HashMap<String, Integer>(); //<Entry ID, arb index>
+    private Map<String, Column> columns  = new HashMap<String, Column>(); //list of columns (key = name, value = column object)
 
+    //ID hashmap: key = arbuitary value for operation, value = original database key to map onto database.
+    //I guess display key can be a whole other column.
     public Table() {
-        //Define starting columns here
+        addColumn("IDs", "int", true);
     }
 
     //Creates a new column
-    //Then when retriving a table to save, you can go forEach and add a column.
-    public void addColumn(String name, String type) {
-        for (Column column : columns){
-            //If a column of that data type is available use it.
-            if (column.getType().equals(type)) {
-                column.addColumn(name);
-                break;
-            }
+    public void addColumn(String name, String type, boolean isNew) {
+        columns.put(name, new Column(type, isNew));
+    }
+
+    //Creates a new index for a new row (database's row's current ID)
+    public void newRow(int originID) {
+        columns.get("IDs").addEntry(columns.get("IDs").size()+1, originID);
+    }
+
+    //New row without an originID (row not formally present in database)
+    public void newRow(){
+        columns.get("IDs").addEntry(columns.get("IDs").size()+1, null);
+    }
+
+    //Adding new entry (row + column intersection)
+    public void newEntry(int ID, T value, String type, String columnName) {
+        //Check row is stored. If not, create.
+        if (!columns.get("IDs").containsKey(ID)) {
+            newRow(ID);
         }
-        //If no column of that data type is found, make one.
-        columns.add(new Column(type));
+        //Check column is present. If not, create.
+        if (!columns.containsKey(columnName)) {
+            addColumn(columnName, type, false);
+        }
+        //Both row and column are available. Add entry.
+        columns.get("columnName").addEntry(ID, value);
     }
 }
 
