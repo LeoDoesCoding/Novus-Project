c2f9b6ba4b8bb04ed322291f04cfe037702f88a5
Leonie Chamberlain <Leoniechamberlain@gmail.com>
2023-11-09 12:59:15 +0000

    Demo Version
    The assessed version of the project.

--- Code/DB-Project/src/main/java/Column.java
+++ Code/DB-Project/src/main/java/Column.java
@@ -1,7 +1,4 @@
-import javafx.util.Pair;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Column<T> {
@@ -35,25 +32,4 @@ public class Column<T> {
     public void setName(String name){this.name = name;}
     public boolean isNew(){return this.isNew;}
     public Map<String, T> getEntries() { return this.entries; }
-
-    //Gets one entry, returns and deletes it from map
-    public Pair<String, String> getReturnString(String oldName) {
-        /*HashMap<Integer, String> toReturn = new HashMap<Integer, String>(entries.size());
-        char apo = '\0';
-
-        //If it is a string column, it needs an appostrophe for the value
-        if (this.type == 12) {
-            apo = '\'';
-        }
-
-        //For each entry, put it in the hashmap as ID, "Columnname = value"
-        for (Map.Entry<Integer, T> entry : entries.entrySet()) {
-            toReturn.put(entry.getKey(), oldName + " = " + apo +  entry.getValue() + apo + ","); //ID, "columnName = value"
-        }*/
-
-        Map.Entry<String, T> entry = entries.entrySet().iterator().next();
-        Pair<String, String> toReturn = new Pair<>(entry.getKey(), entry.getValue().toString());
-        entries.remove(entry);
-        return toReturn;
-    }
 }
